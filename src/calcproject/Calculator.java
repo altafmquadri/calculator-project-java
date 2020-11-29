43c2d59cae6e3e313ead45b22124fcf747d9b98c
Altaf Quadri <altafmquadri@gmail.com>
2020-11-29 14:47:52 -0500

    fix formatting of Menu and Calculator

--- src/calcproject/Calculator.java
+++ src/calcproject/Calculator.java
@@ -10,30 +10,47 @@ public class Calculator {
 	public static void main(String[] args) {
 
 		Menu m = new Menu();
+		double result;
+		System.out.println("Welcome to the calculator program!");
 		
 		do {
 			m.displayMenu();
 			
 			switch (m.choice) {
 			case 1:
-				System.out.println(add(m.a, m.b));
+				result = add(m.a, m.b);
+				System.out.println("You asked to add two numbers:");
+				System.out.println(m.a + " + " +m.b+ " = " +result);
+				System.out.println();
 				break;
 			case 2:
-				System.out.println(subtract(m.a, m.b));
+				result = subtract(m.a, m.b);
+				System.out.println("You asked to subtract two numbers:");
+				System.out.println(m.a + " - " +m.b+ " = " +result);
+				System.out.println();
 				break;
 			case 3:
-				System.out.println(multiply(m.a, m.b));
+				result = multiply(m.a, m.b);
+				System.out.println("You asked to multiply two numbers:");
+				System.out.println(m.a + " * " +m.b+ " = " +result);
+				System.out.println();
 				break;
 			case 4:
-				System.out.println(divide(m.a, m.b));
+				result = divide(m.a, m.b);
+				System.out.println("You asked to divide two numbers:");
+				System.out.println(m.a + " / " +m.b+ " = " +result);
+				System.out.println();
+				break;
+			case 5:
 				break;
 			default:
 				System.out.println("Please select a valid choice!");
+				System.out.println();
 			}
 			
 		} while(m.choice != 5);
 		
-		
+		System.out.println("Thank you for using the calculator!");
 	}
 
 	public static double add(double a, double b) {
--- src/calcproject/Menu.java
+++ src/calcproject/Menu.java
@@ -10,19 +10,25 @@ public class Menu {
 	public void displayMenu() {
 		Scanner scan = new Scanner(System.in);
 		Inputs inp = new Inputs();
-		System.out.println("Enter two numbers: ");
-		
-		a = inp.setA(scan.nextDouble());
-		b = inp.setB(scan.nextDouble());
-		
+
+		System.out.println();
 		System.out.println("What operation would you like to perform?");
 		System.out.println("(1) Add");
 		System.out.println("(2) Subtract");
 		System.out.println("(3) Multiply");
 		System.out.println("(4) Divide");
 		System.out.println("(5) Exit");
-		
-		choice = inp.setChoice(scan.nextByte());		
-		}
-	}
+		System.out.println("Please choose from the above selection: ");
 
+		choice = inp.setChoice(scan.nextByte());
+
+		if (choice != 5 && (choice == 1 || choice == 2 || choice == 3 || choice == 4)) {
+			System.out.println("Enter your first number: ");
+			a = inp.setA(scan.nextDouble());
+			System.out.println("Enter the second number: ");
+			b = inp.setB(scan.nextDouble());
+
+		}
+
+	}
+}
